940e4bbe8c75ad7319e6b5d3314dbe5d7d593d34
fedefasani <fedefasani@gmail.com>
2023-06-28 21:03:25 -0300

    fixs zoom-range-page

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.spec.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.spec.ts
@@ -1,6 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ElementRef } from '@angular/core';
-import { LngLat, Map } from 'mapbox-gl';
 import { ZoomRangePageComponent } from './zoom-range-page.component';
 
 describe('ZoomRangePageComponent', () => {
@@ -9,7 +7,7 @@ describe('ZoomRangePageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ZoomRangePageComponent],
+      declarations: [ZoomRangePageComponent]
     }).compileComponents();
   });
 
@@ -19,79 +17,37 @@ describe('ZoomRangePageComponent', () => {
     fixture.detectChanges();
   });
 
-  afterEach(() => {
-    // Limpia el mapa después de cada prueba
-    component.map?.remove();
+  it('debería crear el componente', () => {
+    expect(component).toBeTruthy();
   });
 
-  it('debería inicializar el mapa después de la vista inicializada', () => {
-    component.divMap = new ElementRef(document.createElement('div'));
-    component.ngAfterViewInit();
-
-    expect(component.map).toBeDefined();
+  it('debería inicializar el mapa', () => {
+    expect(component.mapa).toBeDefined();
   });
 
-  it('debería asignar el zoom al valor actual cuando se produce el evento de zoom', () => {
-    component.divMap = new ElementRef(document.createElement('div'));
-    component.ngAfterViewInit();
-
-    const mockZoom = 12;
-    component.map?.setZoom(mockZoom);
-    component.map?.fire('zoom');
-
-    expect(component.zoom).toEqual(mockZoom);
+  it('debería tener un nivel de zoom inicial de 10', () => {
+    expect(component.zoomLevel).toEqual(10);
   });
 
-  it('debería llamar a zoomTo(18) cuando el evento de zoomend se dispara con un zoom menor a 18', () => {
-    component.divMap = new ElementRef(document.createElement('div'));
-    component.ngAfterViewInit();
-
-    const zoomSpy = spyOn(component.map!, 'zoomTo');
-    component.map?.fire('zoomend', { target: { getZoom: () => 17 } });
-
-    expect(zoomSpy).toHaveBeenCalledWith(18);
+  it('debería tener un centro inicial definido', () => {
+    expect(component.center).toEqual([-63.24314235553986, -32.410705910563124]);
   });
 
-  it('debería actualizar currentLngLat cuando ocurre el evento de movimiento del mapa', () => {
-    component.divMap = new ElementRef(document.createElement('div'));
-    component.ngAfterViewInit();
-
-    const mockLngLat = new LngLat(-74.10380784179445, 4.651165392795477);
-    component.map?.setCenter(mockLngLat);
-    component.map?.fire('move');
-
-    expect(component.currentLngLat).toEqual(mockLngLat);
+  it('debería cambiar el nivel de zoom al llamar a zoomCambio', () => {
+    const newZoom = '12';
+    component.zoomCambio(newZoom);
+    expect(component.zoomLevel).toEqual(Number(newZoom));
   });
 
-  it('debería incrementar el zoom al llamar a zoomIn', () => {
-    component.divMap = new ElementRef(document.createElement('div'));
-    component.ngAfterViewInit();
-
-    const initialZoom = component.zoom;
+  it('debería incrementar el nivel de zoom al llamar a zoomIn', () => {
+    const initialZoom = component.zoomLevel;
     component.zoomIn();
-
-    expect(component.zoom).toBeGreaterThan(initialZoom);
+    expect(component.zoomLevel).toEqual(initialZoom + 1);
   });
 
-  it('debería reducir el zoom al llamar a zoomOut', () => {
-    component.divMap = new ElementRef(document.createElement('div'));
-    component.ngAfterViewInit();
-
-    const initialZoom = component.zoom;
+  it('debería decrementar el nivel de zoom al llamar a zoomOut', () => {
+    const initialZoom = component.zoomLevel;
     component.zoomOut();
-
-    expect(component.zoom).toBeLessThan(initialZoom);
-  });
-
-  it('debería asignar el zoom al valor proporcionado y llamar a zoomTo con ese valor al llamar a zoomChanged', () => {
-    component.divMap = new ElementRef(document.createElement('div'));
-    component.ngAfterViewInit();
-
-    const mockZoom = 15;
-    const zoomToSpy = spyOn(component.map!, 'zoomTo');
-    component.zoomChanged(mockZoom.toString());
-
-    expect(component.zoom).toEqual(mockZoom);
-    expect(zoomToSpy).toHaveBeenCalledWith(mockZoom);
+    expect(component.zoomLevel).toEqual(initialZoom - 1);
   });
 });
--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,67 +1,86 @@
 import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
-import { LngLat, Map } from 'mapbox-gl';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
-  styleUrls: ['./zoom-range-page.component.css']
+  styleUrls: ['./zoom-range-page.component.css'],
+  styles: [`
+    .mapa-container {
+      width: 100%;
+      height: 100%;
+    }
+    
+    .row {
+      background-color: white;
+      border-radius: 5px;
+      bottom: 30px;
+      left: 30px;
+      padding: 10px;
+      position: fixed;
+      z-index: 999;
+      width: 400px;
+    }
+  `]
 })
 export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
 
+  // referencia local
+  @ViewChild('mapa') divMapa!: ElementRef
 
-  @ViewChild('map') divMap?: ElementRef;
-
-  public zoom: number = 10;
-  public map?: Map;
-  public currentLngLat: LngLat = new LngLat(-74.10380784179445, 4.651165392795477);
-
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 10;
+  center: [ number, number ] = [ -63.24314235553986,-32.410705910563124 ]
+  
+  constructor() {}
+  
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () => {});  
+    this.mapa.off('zoomed', () => {});
+    this.mapa.off('move', () => {});
+  }
 
   ngAfterViewInit(): void {
-
-    if ( !this.divMap ) throw 'El elemento HTML no fue encontrado';
-
-    this.map = new Map({
-      container: this.divMap.nativeElement, // container ID
-      center: this.currentLngLat,
-      zoom: this.zoom, // starting zoom
+    
+    // console.log('AfterViewInit',this.divMapa);
+  
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: this.center,
+      zoom: this.zoomLevel
     });
 
-    this.mapListeners();
-  }
+    //- EventListeners -
 
-  ngOnDestroy(): void {
-    this.map?.remove();
-  }
-
-  mapListeners() {
-    if ( !this.map ) throw 'Mapa no inicializado';
-
-    this.map.on('zoom', (ev) => {
-      this.zoom = this.map!.getZoom();
+    this.mapa.on('zoom', (ev) => {
+      this.zoomLevel = this.mapa.getZoom();
     });
 
-    this.map.on('zoomend', (ev) => {
-      if ( this.map!.getZoom() < 18 ) return;
-      this.map!.zoomTo(18);
+    this.mapa.on('zoomend', (ev) => {
+      if ( this.mapa.getZoom() > 18 ){
+        this.mapa.zoomTo( 18 );
+      }
     });
 
-    this.map.on('move', () => {
-      this.currentLngLat = this.map!.getCenter();
-    });
+    this.mapa.on('move', (event) =>{
+      const target = event.target;
+      const { lng, lat } = target.getCenter();
+      this.center = [lng, lat]
+    })
 
-  }
-
-  zoomIn() {
-    this.map?.zoomIn();
+    //--
   }
 
   zoomOut() {
-    this.map?.zoomOut();
+    this.mapa.zoomOut();
   }
 
-  zoomChanged( value: string ) {
-    this.zoom = Number(value);
-    this.map?.zoomTo( this.zoom );
+  zoomIn() {
+    this.mapa.zoomIn();
   }
 
+  zoomCambio( valor: string ){
+    this.mapa.zoomTo( Number(valor) );
+  }
 
 }
